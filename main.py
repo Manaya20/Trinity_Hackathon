19bac042552f6269479d954dd88356ff82b3a0ef
Manaya20 <pachpor.manaya@gmail.com>
2024-02-20 10:23:58 +0530

    initialise

---
+++ main.py
@@ -0,0 +1,10 @@
+#Objective: To understand the impact of Payers(Insurers) on the treatment preference of a patient and physician for a given market. The Insights should help a pharmaceutical company to target key Payers or Plans to engage in a meaningful discussion for adding their drug as a preferred drug to the specfic payer/plan. Summary can be further breakdown by geography, patients age, payment type, etc., Additionally, Physician's preference of a drug by each plan will also helps in promoting through different channels.
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import os
+
+df_claims_and_patients = pd.read_csv("/Users/manayapachpor/Desktop/Trinity_Hackathon/claims_and_patient.csv.xlsx")
+df_
